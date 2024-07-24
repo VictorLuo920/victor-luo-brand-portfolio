1a2cc962cd6a229fbf562ab84d21ff3e0a8f3d97
Victor Luo <victorluo920@gmail.com>
2024-07-24 14:07:54 -0700

    figured out semantic tokens to control color mode variable values

--- src/lib/styles/theme/index.ts
+++ src/lib/styles/theme/index.ts
@@ -4,12 +4,14 @@ import { colors } from './colors';
 import { components } from './components';
 import { config } from './config';
 import { fonts } from './fonts';
+import { semanticTokens } from './semantic-tokens';
 
 const customTheme = extendTheme({
   fonts,
   colors,
   config,
   components,
+  semanticTokens,
 });
 
 export default customTheme;
---
+++ src/lib/styles/theme/semantic-tokens.ts
@@ -0,0 +1,6 @@
+export const semanticTokens = {
+  colors: {
+    'chakra-body-text': { _light: 'gray.800', _dark: 'whiteAlpha.900' },
+    'chakra-body-bg': { _light: 'rgb(247, 242, 238)', _dark: 'gray.800' },
+  },
+};
