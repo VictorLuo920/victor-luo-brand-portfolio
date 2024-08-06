87a84b8e440c8976978bdf8f3eea07f109b25414
Victor Luo <victorluo920@gmail.com>
2024-08-06 14:12:13 -0700

    fixed typing/null error in slug page

--- src/lib/pages/writing/[slug]/index.tsx
+++ src/lib/pages/writing/[slug]/index.tsx
@@ -10,7 +10,7 @@ const Post = async ({ params }: { params: { slug: string } }) => {
     .select()
     .eq('title', `${slugToStr(params.slug)}`);
 
-  return (
+  return post ? (
     <Flex
       direction="column"
       alignItems="center"
@@ -32,6 +32,33 @@ const Post = async ({ params }: { params: { slug: string } }) => {
           textAlign="initial"
           mb={4}
         >{`published on ${formatDate(post[0]?.created_at)}`}</Text>
+      </Grid>
+      <Stack gap={4} w={{ base: '100%', md: '75%' }}>
+        <Button variant="outline" as={Link} href="/">
+          Home Page
+        </Button>
+        <Button as={Link} href="/writing">
+          My Writing
+        </Button>
+      </Stack>
+    </Flex>
+  ) : (
+    <Flex
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="70vh"
+      gap={4}
+      mb={8}
+      w="full"
+    >
+      <Grid textAlign="center">
+        <Heading as="h1" size="4xl" mb={4}>
+          No post here!
+        </Heading>
+        <Text fontSize="md" whiteSpace="pre-line" textAlign="initial" mb={4}>
+          Cannot find this post!
+        </Text>
       </Grid>
       <Stack gap={4} w={{ base: '100%', md: '75%' }}>
         <Button variant="outline" as={Link} href="/">
